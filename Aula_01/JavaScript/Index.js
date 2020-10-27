8f8f0d79155d7209791abbc47ec257fc9afa61c6
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-10-27 12:10:14 -0300

    Example of first class functions

--- Aula_01/JavaScript/Index.js
+++ Aula_01/JavaScript/Index.js
@@ -1,5 +1,27 @@
-// Exemplo de uso de funções simultâneas assíncronas  //  Example of using asynchronous simultaneous functions //
+/*// Exemplo de uso de funções simultâneas assíncronas  //  Example of using asynchronous simultaneous functions //
 
+
+
+setTimeout(function() {
+    console.log("oi 1")
+}, 5000)
+
+const myFunction = function () {
+    console.log("oi 3")
+}
+
+setTimeout(myFunction, 5000)
+
+console.log('oi 2')
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////*/
+
+// Exemplo de funções de primeira classe  //  Example of first class functions //
+
+// funçoes se tornam um tipo de dado e podem ser armazenado em variaveis;
 
 
 setTimeout(function() {
