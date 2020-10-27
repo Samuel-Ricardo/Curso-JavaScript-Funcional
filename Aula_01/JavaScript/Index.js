6b01aeb08432ddd7dd6f27c87236006caa904924
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-10-27 12:07:51 -0300

    Example of using asynchronous simultaneous functions

--- Aula_01/JavaScript/Index.js
+++ Aula_01/JavaScript/Index.js
@@ -1,1 +1,21 @@
+// Exemplo de uso de funções simultâneas assíncronas  //  Example of using asynchronous simultaneous functions //
+
+
+
+setTimeout(function() {
+    console.log("oi 1")
+}, 5000)
+
+const myFunction = function () {
+    console.log("oi 3")
+}
+
+setTimeout(myFunction, 5000)
+
+console.log('oi 2')
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
 
