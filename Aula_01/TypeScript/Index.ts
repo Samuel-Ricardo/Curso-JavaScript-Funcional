c74a75f9ca959984c2068ee0e7aeaa27ca20f55f
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-10-27 12:20:06 -0300

    Example of first class functions

---
+++ Aula_01/TypeScript/Index.ts
@@ -0,0 +1,14 @@
+// Exemplo de funções de primeira classe  //  Example of first class functions //
+
+// funçoes se tornam um tipo de dado e podem ser armazenado em variaveis;
+
+
+const getName: Function = function (): void {
+
+    console.log('Felipe')
+}
+
+getName();
+
+
+//////////////////////////////////////////////////////////////////////////////////
