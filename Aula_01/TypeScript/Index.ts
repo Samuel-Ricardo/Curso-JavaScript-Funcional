4f7c39966af37f2c695613bffa3c64dbe63ed82d
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-10-28 11:13:35 -0300

    Arrow function type script

--- Aula_01/TypeScript/Index.ts
+++ Aula_01/TypeScript/Index.ts
@@ -31,3 +31,50 @@ setTimeout(function(): void{
 
 //////////////////////////////////////////////////////////////////////////////////
 */
+
+// Exemplo de Arrow Functions  //  Example of Arrow Functions //
+
+//  são na verdade um resumo da sintaxe de uma função, sendo capaz de resumir uma função em 1 linha por exemplo  //  are actually a syntax summary of a function, being able to summarize a function in 1 line for example // functions that receive  //
+
+
+const test: Function = () => {return console.log("1")}
+const test: Function = () => console.log("1")     //  se for apenas 1 linha de código pode ser feita dessa maneira e ele automáticamente retorna o resultado  //  if it is, only 1 line can be made this way, and it automatically returns the result  //
+
+setTimeout(test, 1000)
+
+console.log(2)
+
+//-------------------------------------------------------------------------------------------------//
+
+const loading: number = setInterval( () => console.log('loading...'), 500)
+
+//  Como este possui mais de 1 linha deverá ser feito desta forma  //  As it has more than 1 line it should be done this way  //
+
+setTimeout((): void => { 
+    clearInterval(loading)
+    console.log('loaded')
+},3000)
+
+//-------------------------------------------------------------------------------------------------//
+
+// 1º Maneira // 1º way //
+
+const message: Function = function(name: string) {
+    return "Hello" + name
+}
+
+console.log (message('Felipe'))
+
+// 2º Maneira // 2º way //
+
+const message: Function =  (name:string):string => "Hello" + name
+
+console.log (message('Felipe')) 
+
+//-------------------------------------------------------------------------------------------------//
+
+const som = (x:number, y:number):number => x + y
+
+console.log(som(4, 7))
+
+//////////////////////////////////////////////////////////////////////////////////
