9bd7fcf87af545d1556d3e6a09cdb8ee7c4cd7f5
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-10-28 11:50:40 -0300

    pure and impure funcin

--- Aula_01/JavaScript/Index.js
+++ Aula_01/JavaScript/Index.js
@@ -51,7 +51,6 @@ setTimeout(function(){
 
 
 //////////////////////////////////////////////////////////////////////////////////
-*/
 
 // Exemplo de Arrow Functions  //  Example of Arrow Functions //
 
@@ -99,3 +98,57 @@ const som = (x, y) => x + y
 console.log(som(4, 7))
 
 //////////////////////////////////////////////////////////////////////////////////
+*/
+
+// Exemplo de funções Puras e Impuras  //  Example of Pure and Impure functions //
+
+// Exemplo de funções Puras e Impuras Estão diretamente relacionada com o conceito de "IMUTABILIDADE" do paradigma funcional // Example of Pure and Impure functions They are directly related to the "IMUTABILITY" concept of the functional paradigm  //
+
+
+// Funçao Impura // Impure functions //
+
+//  Modifica ou Trabalha com estados que podem ser modificados  //  Modify or Work with states that can be modified  //
+
+//-------------------------------------------------------------------------------------------------//
+
+// o resultado da soma é incerto e depende do valor/estado de x  // the result of the sum is uncertain and depends on the value / state of x //
+
+const x = 10
+
+const impureSum = y => x + y 
+
+console.log(impureSum(3))
+
+//-------------------------------------------------------------------------------------------------//
+// Aqui pode ser mais claro de observar, pois ele vai alterar diretamente o estado da variavel "Z", pois quando eu chamar a primeira vez o resultado será "14"  e este passa a ser o novo estado de 'Z', e quando eu chamar novamente eu terei um resultado diferente do esperado (que seria 15), pois o "Z" nao é mais igual a "10" e sim a "14" devido a ultima chamada da funçao, se por exemplo ambas funçoes fossem executadas simultaneamente poderia quebrar sua aplicaçao pois o resultado depende de qual chamada for executada primeiro, tornando assim o comportamento do seu app instável  //
+//-------------------------------------------------------------------------------------------------//
+// Here it can be clearer to observe, as it will directly change the state of the variable "Z", because when I call the first time the result will be "14" and this becomes the new state of 'Z', and when i call again i will have a different result than expected (which would be 15), because the "Z" is no longer equal to "10" but "14" due to the last function call, if for example both functions were executed simultaneously it could break your application because the result depends on which call is executed first, thus making your app's behavior unstable //
+//-------------------------------------------------------------------------------------------------//
+
+let z = 10
+const impureSumInz = y => z += y
+
+console.log(impureSumInz(4))
+console.log(impureSumInz(5))
+
+
+//-------------------------------------------------------------------------------------------------//
+
+// Funçao Pura // Pure functions //
+
+//  Utiliza o conceito de “Imutabilidade”, ou seja, nenhum estado será alterado dentro desta função, os dados são passados como parâmetro, consequentemente a função pode ser isolada  //  It uses the concept of "Immutability", that is, no state will be changed within this function, the data are passed as a parameter, consequently the function can be isolated  //
+
+//-------------------------------------------------------------------------------------------------//
+
+// o resultado e a execuçao da soma nao altera nenhum estado fora da funçao  // the result of the sum is uncertain and depends on the value / state of x //
+
+const pureSum = (x,y) => x + y 
+
+console.log(pureSum(7, 5))
+console.log(pureSum(10, 5))
+
+//-------------------------------------------------------------------------------------------------//
+// Pode-se notar que independente da ordem em que as funções são executadas, o resultado de uma não influenciará na outra, tendo assim os resultados esperados (Ex: 10 + 5 = 15)  //
+//-------------------------------------------------------------------------------------------------//
+//  It can be noted that regardless of the order in which the functions are executed, the result of one will not influence the other, thus having the expected results (Ex: 10 + 5 = 15) //
+//-------------------------------------------------------------------------------------------------//
